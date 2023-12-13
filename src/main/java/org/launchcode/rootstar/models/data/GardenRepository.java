cb93240ab515da2c5598954f1427b4dce5506ad6
Andrew <awoodcoc@indiana.edu>
2023-12-12 20:05:37 -0600

    reconfigured repositories with JpaRepository extension

--- src/main/java/org/launchcode/rootstar/models/data/GardenRepository.java
+++ src/main/java/org/launchcode/rootstar/models/data/GardenRepository.java
@@ -1,9 +1,12 @@
 package org.launchcode.rootstar.models.data;
 
 import org.launchcode.rootstar.models.Garden;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface GardenRepository extends CrudRepository <Garden, Integer> {
+public interface GardenRepository extends JpaRepository<Garden, Integer> {
+    Optional<Garden> findById(int id);
 }
--- src/main/java/org/launchcode/rootstar/models/data/PlantRepository.java
+++ src/main/java/org/launchcode/rootstar/models/data/PlantRepository.java
@@ -1,9 +1,9 @@
 package org.launchcode.rootstar.models.data;
 
 import org.launchcode.rootstar.models.Plant;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlantRepository extends CrudRepository<Plant, Integer> {
+public interface PlantRepository extends JpaRepository<Plant, Integer> {
 }
