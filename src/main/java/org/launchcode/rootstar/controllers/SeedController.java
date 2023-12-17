5fbf3b58d748334946f728ff24ecc40658c02fd8
Andrew <awoodcoc@indiana.edu>
2023-12-16 18:46:06 -0600

    refactored SeedController to upgrade

--- src/main/java/org/launchcode/rootstar/controllers/SeedController.java
+++ src/main/java/org/launchcode/rootstar/controllers/SeedController.java
@@ -3,9 +3,13 @@ package org.launchcode.rootstar.controllers;
 import org.launchcode.rootstar.models.Seed;
 import org.launchcode.rootstar.service.SeedService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/seeds")
@@ -15,15 +19,29 @@ public class SeedController {
     @Autowired
     public SeedService seedService;
 
-    @PostMapping("/add")
-    public String addSeed(@RequestBody Seed seed) {
-        seedService.addSeed(seed);
-        return "New seed is added.";
+    // GET MAPPING
+    @GetMapping("/{id}")
+    public ResponseEntity<Seed> getEventById(@PathVariable int id) {
+        Optional<Seed> event = seedService.getSeedById(id);
+        return event.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
-    // TODO: Refactor if needed. This was how the YouTube video said to do it LOL
     @GetMapping("view-seeds")
     public List<Seed> viewSeeds() {
         return seedService.getAllSeeds();
+    }
+
+    // POST MAPPING
+    @PostMapping("/add")
+    public ResponseEntity<Seed> addSeed(@RequestBody Seed seed) throws URISyntaxException {
+        Seed savedSeed = seedService.addSeed(seed);
+        return ResponseEntity.created(new URI("/seeds/" + savedSeed.getId())).body(savedSeed);
+    }
+
+    // DELETE MAPPING
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteSeed(@PathVariable int id) {
+        seedService.deleteSeed(id);
+        return ResponseEntity.ok().build();
     }
 }
