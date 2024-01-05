083fdaf018e8016528f308adfa9ae67fc7d3f26f
Riley <ririwilliams.rw@gmail.com>
2024-01-04 19:44:22 -0600

    Working on sorting

--- src/main/java/org/launchcode/rootstar/models/SortFunctions.java
+++ src/main/java/org/launchcode/rootstar/models/SortFunctions.java
@@ -1,4 +1,17 @@
 package org.launchcode.rootstar.models;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import org.launchcode.rootstar.models.data.GardenRepository;
+import org.springframework.data.domain.Sort;
+
+import java.util.*;
+
 public class SortFunctions {
+    public List<Garden> sortAscending() {
+        Sort sort = Sort.by(Sort.Direction.ASC, "seed", "plant");
+        List<Garden> gardens = GardenRepository.findAll(sort);
+
+
+    }
 }
