e7d4db116d179586e46e5fd5917496012fddb15c
Riley <ririwilliams.rw@gmail.com>
2024-01-11 18:34:29 -0600

    Finishing up getWeatherData to retrieve five day waether forecast

--- src/main/java/org/launchcode/rootstar/service/WeatherService.java
+++ src/main/java/org/launchcode/rootstar/service/WeatherService.java
@@ -1,24 +1,66 @@
 package org.launchcode.rootstar.service;
 
-import com.mysql.cj.xdevapi.JsonArray;
-import org.apache.tomcat.util.json.JSONParser;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
+@Service
 public class WeatherService {
 
     public static JSONObject getWeatherData(int locationKey){
-        //get postal code locationKey from API
-        JsonArray locationData = getLocationData(locationKey);
+
+        JSONArray locationData = getLocationData(locationKey);
+
+        //get location key from postal code
+        JSONObject location = (JSONObject) locationData.get(0);
+        String key = (String) location.get("Key");
+
+        //API request URL with location key
+        String urlString = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/&q=28204_PC?apikey=FGZPcs0psWYrremDRCAfgYcOAZhFXxeE&q=" + key;
+
+        try{
+            //call api and get response
+            HttpURLConnection conn = fetchApiResponse(urlString);
+
+            //check response status
+
+            if (conn.getResponseCode() != 200){
+                System.out.println("Error: Could not connect to API");
+                return null;
+            }
+
+            StringBuilder resultsJson = new StringBuilder();
+            Scanner scanner = new Scanner(conn.getInputStream());
+            while(scanner.hasNext()){
+                //read and store into string builder
+                resultsJson.append(scanner.nextLine());
+            }
+            scanner.close();
+            conn.disconnect();
+
+            //parse through data
+            JSONParser parser = new JSONParser();
+            JSONObject resultsJsonObj = (JSONObject) parser.parse(String.valueOf(resultsJson));
+
+            //retrieve five day data
+            JSONObject fiveDay = (JSONObject) resultsJsonObj.get("DailyForecasts");
+
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     //retrieves location key using postal codes
-    private static JsonArray getLocationData(int locationKey){
+    public static JSONArray getLocationData(int locationKey){
         String urlString = "http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=FGZPcs0psWYrremDRCAfgYcOAZhFXxeE&q=" + locationKey ;
 
         try {
@@ -48,10 +90,13 @@ public class WeatherService {
                 JSONObject resultsJsonObj = (JSONObject) parser.parse(String.valueOf(resultJson));
 
                 JSONArray locationData = (JSONArray) resultsJsonObj.get("results");
+                return locationData;
             }
-        }catch(Exception e){
+        } catch(Exception e){
             e.printStackTrace();
         }
+        //return null if can't find location
+        return null;
     }
 
     private static HttpURLConnection fetchApiResponse(String urlString){
