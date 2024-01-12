36f855e75f52ecad4661647e6d34bec4f91a170e
Andrew <awoodcoc@indiana.edu>
2024-01-11 22:03:57 -0600

    search frontend work begun. first draft of searchcontroller

---
+++ src/main/java/org/launchcode/rootstar/controllers/SearchController.java
@@ -0,0 +1,32 @@
+package org.launchcode.rootstar.controllers;
+
+
+import org.launchcode.rootstar.models.Garden;
+import org.launchcode.rootstar.models.data.GardenRepository;
+import org.launchcode.rootstar.service.SearchFunctions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("search")
+@CrossOrigin
+public class SearchController {
+
+    @Autowired
+    private SearchFunctions searchFunctions;
+
+    @Autowired
+    private GardenRepository gardenRepository;
+
+    @GetMapping
+    // method findGarden utilizes method findByValue from SearchFunctions.java line 46
+    public ResponseEntity<List<Garden>> findGarden(@RequestParam String query) {
+        List<Garden> searchResults = searchFunctions.findByValue(query, gardenRepository.findAll());
+        return new ResponseEntity<>(searchResults, HttpStatus.OK);
+    }
+
+}
